971a9b46ac08e72e9fe5fc7a38555d0581dd265f
adityayadav176 <ay6598aditya@gmail.com>
2026-01-27 21:24:19 +0530

    username, email,password must be valid this make from express-validator

--- routes/auth.js
+++ routes/auth.js
@@ -1,13 +1,27 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/User.js');
+const { body, validationResult } = require('express-validator');
 
 // create a User using: POST "/api/auth/". Doesn't require Auth  
-router.post('/', (req, res) => {
-    console.log(req.body);
-    const user = User(req.body);
-    user.save()
-    res.send(req.body);
+router.post('/',[
+    body('email', 'Enter a valid email').isEmail(),
+    body('name', 'Please enter a unique value for email').isLength({min: 5}),
+    body('password', 'Password must be atleast 8 Characters').isLength({min: 8}),
+], (req, res) => {
+    const error = validationResult(req);
+    if(!error.isEmpty()){
+        return res.status(400).json({error: error.array()});
+    }
+    User.create({
+        name: req.body.name,
+        password: req.body.password,
+        email: req.body.email,
+    }).then(user => res.json(user))
+    .catch(err => {console.log(err)
+        res.json({error:" Please Enter a unique value for email"})
+    })
+      
 })
 
 module.exports = router
