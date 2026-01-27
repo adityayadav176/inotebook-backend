8560af85757957ae3aabf34ba7f7ff8522e75b61
adityayadav176 <ay6598aditya@gmail.com>
2026-01-27 23:37:41 +0530

    Some Error Occured Internally to Catch This Error Use Try Catch Block

--- routes/auth.js
+++ routes/auth.js
@@ -21,11 +21,7 @@ router.post('/createuser',[
             return res.status(400).json({ error: "Sorry a user with this email already exists" })
         }
         //Create a new user
-        user = await User.create({
-            name: req.body.name,
-            password: req.body.password,
-            email: req.body.email,
-        })  
+      
         res.json(user)
     }catch (error){
         res.status(500).send("Some error occured");
