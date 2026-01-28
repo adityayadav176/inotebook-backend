801464c1363da347788d97b587805b979f79ef06
adityayadav176 <ay6598aditya@gmail.com>
2026-01-28 14:34:36 +0530

    Creating login Endpoint Using Post request

--- routes/auth.js
+++ routes/auth.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/User.js');
 const { body, validationResult } = require('express-validator');
-// const bcrypt = require('bcryptjs');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
 const JWT_SECRET = "Mynameisaditya"
